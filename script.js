3541049c1fc17019d65c3d36cf684ee157c61742
yqni13 <65848806+yqni13@users.noreply.github.com>
2024-04-15 22:50:03 +0200

    draggable song list vers #1

--- script.js
+++ script.js
@@ -25,6 +25,33 @@ function addDemoList() {
     }
 }
 
+/*-----------------------------------------------------------------------------------------------------------*/
+
+var _el;
+
+function dragOver(e) {
+  if (isBefore(_el, e.target))
+    e.target.parentNode.insertBefore(_el, e.target);
+  else
+    e.target.parentNode.insertBefore(_el, e.target.nextSibling);
+}
+
+function dragStart(e) {
+  e.dataTransfer.effectAllowed = "move";
+  e.dataTransfer.setData("text/plain", null); // Thanks to bqlou for their comment.
+  _el = e.target;
+}
+
+function isBefore(el1, el2) {
+  if (el2.parentNode === el1.parentNode)
+    for (var cur = el1.previousSibling; cur && cur.nodeType !== 9; cur = cur.previousSibling)
+      if (cur === el2)
+        return true;
+  return false;
+}
+
+/*-----------------------------------------------------------------------------------------------------------*/
+
 function removeAll() {
     // TODO remove all displayed elements in list?
 
